293db91bf9f8bd319bee3b3291278a53fee1d25f
Damien <damien@dam.io>
2016-09-19 11:37:20 +0200

    first proto: map okayish, extension okayish

---
+++ serv.py
@@ -0,0 +1,87 @@
+import functools
+import requests, json
+from bs4 import BeautifulSoup
+
+from geopy.geocoders import Nominatim
+geolocator = Nominatim()
+
+import flask
+from flask_cache import Cache
+app = flask.Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'filesystem', 'CACHE_DIR': 'cache'})
+
+
+@cache.memoize(1000)
+def geolocate(place):
+    if place:
+        print('geolocate', place)
+        location = geolocator.geocode(place)
+        if location:
+            return {
+                'lat': location.latitude,
+                'lng': location.longitude,
+            }
+
+
+@cache.memoize(1000)
+def fetch_items(url):
+    print('fetch items', url)
+    all_found = []
+
+    page = 1
+    while True:
+        print(url)
+        resp = requests.get(url + '&o=' + str(page))
+        print(resp)
+        soup = BeautifulSoup(resp.text, 'lxml')
+
+        for item in soup.select('.list_item'):
+            data = {}
+            data['link'] = item.attrs['href']
+            data['title'] = item.select('.item_title')[0].text.strip()
+            img = item.select('.item_imagePic span')
+            if img:
+                data['thumbnail'] = img[0].attrs['data-imgsrc']
+            if item.select('.item_supp'):
+                data['category'] = item.select('.item_supp')[0].text \
+                                        .replace('(pro)', '').strip()
+                place = item.select('.item_supp')[1].text
+                place = ' '.join(x.strip() for x in place.strip().split(' ') if x.strip())
+                data['place'] = place
+            data['coords'] = geolocate(place)
+            price = item.find(class_='item_price')
+            if price:
+                data['price'] = price.text.replace('\xa0', ' ').strip()
+            if item.find(class_='item_absolute'):
+                data['date'] = item.find(class_='item_absolute').text.strip()
+            all_found.append(data)
+        next = soup.find(id='last')
+
+        break
+
+        if next:
+            page += 1
+        else:
+            break
+
+    return all_found
+
+
+@app.route("/items")
+def fetch():
+    items = fetch_items(flask.request.args.get('url'))
+    return flask.jsonify(*items)
+
+
+@app.route("/")
+def index():
+    return app.send_static_file('hello.html')
+
+
+@app.route('/main.js')
+def send_js():
+    return app.send_static_file('hello.js')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/hello.js
@@ -0,0 +1,52 @@
+'use strict';
+
+function getQueryStrings() { 
+  var assoc  = {};
+  var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
+  var queryString = location.search.substring(1); 
+  var keyValues = queryString.split('&'); 
+
+  for(var i in keyValues) { 
+    var key = keyValues[i].split('=');
+    if (key.length > 1) {
+      assoc[decode(key[0])] = decode(key[1]);
+    }
+  } 
+
+  return assoc; 
+} 
+
+var qs = getQueryStrings();
+
+$.getJSON('/items', {url: qs['url']}).then(function(DATA) {
+    L.mapbox.accessToken = 'pk.eyJ1IjoicG93ZXJzaG9wcyIsImEiOiJhYUdRR0t3In0.oTz8RJqED2YEcDRfJYNAOQ';
+    var map = L.mapbox.map('map', 'mapbox.streets', {
+      maxZoom: 10,
+    });
+    var markers = new L.MarkerClusterGroup({
+      animate: false,
+    });
+
+    DATA.forEach(function(a) {
+        if (a.coords) {
+            var marker = new L.Marker([
+              a.coords.lat,
+              a.coords.lng,
+            ], {
+                icon: new L.DivIcon({
+                    className: 'ad-icon',
+                    html: '<h2>' + a.title + (a.price ? (' - ' + a.price) : '') + '</h2>'
+                })
+            });
+            marker.on('click', function(e) {
+              window.open(a.link, '_blank');
+            });
+            markers.addLayer(marker);
+        } else {
+          console.log('ignoring', a.title, a.link)
+        }
+    });
+
+    map.addLayer(markers);
+    map.fitBounds(markers.getBounds());
+})
