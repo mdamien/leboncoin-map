5a9c7a7ee124c2f8fe6a8cb9d99cf85683685f48
Damien <damien@dam.io>
2016-09-22 19:31:52 +0200

    adjust padding to make the button button fit in more cases

--- extension/main.js
+++ extension/main.js
@@ -1,7 +1,7 @@
 if (document.getElementById('searchbutton')) { // verify user is doing a search
     var container = document.querySelector('.list .tabsHeader');
 
-    var btn_html = '<input id="map-btn" value="🌍 Carte" type="submit" class="button-blue full" style="background-color: #666;min-height: initial;line-height: 13px;width: auto;padding: 0px 21px;float: right;margin-left: 18px;">';
+    var btn_html = '<input id="map-btn" value="🌍 Carte" type="submit" class="button-blue full" style="background-color: #666;min-height: initial;line-height: 13px;width: auto;padding: 0px 5px;float: right;margin-left: 10px;">';
     container.insertAdjacentHTML('afterbegin', btn_html);
     var btn = container.querySelector('#map-btn');
     
